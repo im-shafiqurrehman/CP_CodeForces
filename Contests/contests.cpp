64c2dfac1da0fd1d0e01e2d49d092fe826c55e2f
Shafiq Ur Rehman <shafiqurrehmanbscs2022@gmail.com>
2025-01-27 20:42:51 +0500

    daily problem

--- Contests/contests.cpp
+++ Contests/contests.cpp
@@ -2,23 +2,39 @@
 #include <string>
 using namespace std;
 
-int countBlocks(const string& s) {
-    int blocks = 0;
-    for (size_t i = 0; i < s.length(); ++i) {
-        if (s[i] == '1') {
-            ++blocks;
-        }
-    }
-    return blocks;
+void Solve {
+	int n, k ;
+	cin>>n>>k;
+    vector<int> Hallomoi(n);
+    for(int n : Hallomoi){
+    	cin>>Hallomoi[n];
+	};
+	
+	if (k<2 && Hallomoi != sort(Hallomoi.begin() , Hallomoi.end())){
+		cout<<"No";
+	}
+
+	int temp;
+	for(int i=Hallomoi.size(); i>0;i--){
+		for(int j = 0 ; j<i;i++){
+			if(Hallomoi[i] < Hallomoi[i+1]){
+				
+				temp = Hallomoi[i];
+				Hallomoi[i] = Hallomoi[i+1];
+				Hallomoi[i+1] == temp;
+			}							
+			  
+			}
+		}
+	}
+    
 }
 
 int main() {
     int t;
     cin >> t;
     while (t--) {
-        string s;
-        cin >> s;
-        cout << countBlocks(s) << endl;
+        Solve();
     }
     return 0;
 }
